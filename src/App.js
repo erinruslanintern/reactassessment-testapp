380179ca9f245b0595cca4b80e0364fd0aa8fe9c
ErinRuslan <erinsoraya26@gmail.com>
2021-06-10 14:15:43 +0800

    Fetched data from API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
 import UserList from './components/UserList';
-
-//https://jsonplaceholder.typicode.com/users
+import {useEffect} from 'react';
+import {useDispatch} from 'react-redux';
+import {userActions} from './store/user';
 
 function App() 
 {
+  const dispatch = useDispatch();
+
+  useEffect(() =>
+  {
+    const fetchData = async () =>
+    {
+      const res = await fetch('https://jsonplaceholder.typicode.com/users');
+      const resData = await res.json();
+      dispatch(userActions.setFetchedData(resData));
+    }
+
+    fetchData();
+
+  }, [])
+
   return (
     <div className="App">
       <UserList />
--- src/store/user.js
+++ src/store/user.js
@@ -2,33 +2,7 @@ import {createSlice, configureStore} from '@reduxjs/toolkit';
 
 const initUser =
 {
-    users: [
-        {
-            id: 1,
-            name: "Leanne Graham",
-            username: "Bret",
-            email: "Sincere@april.biz",
-            address: 
-            {
-              street: "Kulas Light",
-              suite: "Apt. 556",
-              city: "Gwenborough",
-              zipcode: "92998-3874",
-              geo: 
-              {
-                lat: "-37.3159",
-                lng: "81.1496"
-              }
-            },
-            phone: "1-770-736-8031 x56442",
-            website: "hildegard.org",
-            company: {
-            name: "Romaguera-Crona",
-            catchPhrase: "Multi-layered client-server neural-net",
-            bs: "harness real-time e-markets"
-            }
-        }
-    ]
+    users: []
 }
 
 const userSlice = createSlice(
@@ -42,7 +16,11 @@ const userSlice = createSlice(
             editUser()
             {},
             removeUser()
-            {}
+            {},
+            setFetchedData(state, action)
+            {
+                state.users = action.payload;
+            }
         }
     }
 )
